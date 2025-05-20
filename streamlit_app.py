fc00cb83e9336526fabf64204278544c8e13d994
ijulzx <calladium.missmuffet@gmail.com>
2025-05-20 08:35:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🏆 Welcome to Faiz stuff ⚡")
+st.title("🏆  Welcome to Faiz stuff  ⚡")
 st.write("Dayum, masih belum apa apa cuy")  
 
 st.image("lewandowski.jpg",width=200
