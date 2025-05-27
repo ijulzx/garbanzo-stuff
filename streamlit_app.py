a537f36c45f7d68af4ec8cccb4fe4e4cdba94fc7
ijulzx <calladium.missmuffet@gmail.com>
2025-05-27 07:47:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.title("🏆  Welcome to Faiz stuff  ⚡")
 st.write("Dayum, masih belum apa apa cuy")  
 
 st.image("lewandowski.jpg",width=200)
+
+st.title("Barcelona Trebel Winner")
+st.header("Pengecek angka ganjil\genap")
+
+if (angka % 2) == 0:
+  st.write(f"{angka} adalah bilangan genap")
+else:
+  st.write(f"{angka} adalah bilangan ganjil)
